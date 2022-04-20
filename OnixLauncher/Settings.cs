527f59068e03df59d648758121194593f0d0e725
carlton <carltonrbx@outlook.com>
2022-04-20 16:14:09 -0600

    Progress on file access.

--- OnixLauncher/Settings.cs
+++ OnixLauncher/Settings.cs
@@ -1,7 +1,11 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace OnixLauncher
 {
+    [Serializable]
     public struct Settings
     {
         public bool InsiderMode;
@@ -20,6 +24,25 @@ namespace OnixLauncher
                 GradColor1 = Color.FromArgb(30, 140, 215),
                 GradColor2 = Color.FromArgb(1, 254, 218)
             };
+        }
+
+        public static void Save(Settings settings)
+        {
+            var formatter = new BinaryFormatter();
+            using (var stream = new FileStream(Utils.OnixPath + "\\Settings.dat", FileMode.Create))
+            {
+                formatter.Serialize(stream, settings);
+            }
+        }
+
+        public static Settings Load()
+        {
+            var formatter = new BinaryFormatter();
+            using (var stream = new FileStream(Utils.OnixPath + "\\Settings.dat", FileMode.Open, FileAccess.Read))
+            {
+                var settings = (Settings)formatter.Deserialize(stream);
+                return settings;
+            }
         }
     }
 }
